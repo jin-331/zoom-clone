7f73fc3c211d61426fa10618aa1884bc5212cde2
j.hamada <j.hamada@N0355noMBP.elecom>
2021-06-23 02:20:36 +0900

    [Add] ReactRouter 及びページの実装。

---
+++ src/Pages/EnterPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../App.css"
+
+const EnterPage = () => {
+	return (
+		<div className="EnterPage">
+		<h3>EnterPage</h3>
+		</div>
+	)
+}
+
+export default EnterPage;
